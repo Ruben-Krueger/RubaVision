d670c139d323452493d1edb55b9c4d388e892aa4
Ruben Krueger <34781887+Ruben-Krueger@users.noreply.github.com>
2023-09-24 17:26:52 -0700

    react router

--- rubavision/src/App.js
+++ rubavision/src/App.js
@@ -1,10 +1,31 @@
+// import '@mantine/core/styles.css';
+import { MantineProvider, createTheme } from '@mantine/core';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Start from './start'
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+{/* <MantineProvider theme={theme}>
+</MantineProvider> */}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <button>Begin</button>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Start/>
+        </Route>
+        <Route path="/canvas">
+          <h1>about</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ rubavision/src/start.jsx
@@ -0,0 +1,6 @@
+export default function Start() {
+    return (
+    <div>
+        <h1>start</h1>
+    </div>)
+}
