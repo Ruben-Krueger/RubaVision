4a34d5f976875a544610e3b482d6ef295a409d26
Ruben Krueger <34781887+Ruben-Krueger@users.noreply.github.com>
2023-10-14 13:16:32 -0700

    rm strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import App from './App';
 import nullThrows from 'capital-t-null-throws';
 
 const root = ReactDOM.createRoot(nullThrows(document.getElementById('root')));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Not using "StrictMode here" as in development there is no way to prevent double rendering of components
+// https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-strict-mode
+root.render(<App />);
