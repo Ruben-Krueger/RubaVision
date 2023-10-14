598d4bbc7d3e31719c5eff07caf863b276e01377
Ruben Krueger <34781887+Ruben-Krueger@users.noreply.github.com>
2023-10-14 16:27:31 -0700

    stash

--- src/components/End.tsx
+++ src/components/End.tsx
@@ -1,7 +1,16 @@
-import { Container, Button, Flex, Center, Text, Paper } from '@mantine/core';
+import {
+  Container,
+  Button,
+  Flex,
+  Center,
+  Text,
+  Paper,
+  Space,
+} from '@mantine/core';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Round from '../types/Round';
+import { IconCheck, IconCircleX } from '@tabler/icons-react';
 
 export default function End(): JSX.Element {
   const history = useHistory();
@@ -17,12 +26,25 @@ export default function End(): JSX.Element {
       <Center>
         <Flex gap="md" direction="column">
           <Text size="lg">Round over</Text>
+          {rounds.length === 0 && (
+            <Text size="lg">Uh-oh! We couldn't parse the previous game</Text>
+          )}
           <Text size="md">Total score: {score} </Text>
 
           {rounds.map((round, i) => (
             <Paper key={round.startMS} shadow="xs" p="xl">
-              <Text>Guess: {round.guess ?? '(none)'}</Text>
-              <Text>Answer: {round.answer}</Text>
+              <Flex>
+                {round.guess === round.answer ? (
+                  <IconCheck color="green" />
+                ) : (
+                  <IconCircleX color="red" />
+                )}
+                <Space w="md" />
+                <Flex direction="column">
+                  <Text>Guess: {round.guess ?? '(none)'}</Text>
+                  <Text>Answer: {round.answer}</Text>
+                </Flex>
+              </Flex>
             </Paper>
           ))}
 
